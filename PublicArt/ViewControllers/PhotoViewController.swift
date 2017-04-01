0cf1c69cf99a009bda214bb4a6b532517b2c317c
Michael Kinney <michaelakinney@comcast.net>
2017-03-31 17:50:45 -0700

    Add scrollview to photo view controller

--- PublicArt/ViewControllers/PhotoViewController.swift
+++ PublicArt/ViewControllers/PhotoViewController.swift
@@ -15,14 +15,19 @@ class PhotoViewController: UIViewController {
     var page: Int = 0
     var noPhotoImage: UIImage?
     
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        scrollView.delegate = self
+        scrollView.minimumZoomScale = 1.0
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
+        scrollView.zoomScale = 1.0
+        scrollView.contentSize = imageView.bounds.size
+
         if let photo = photo {
             activityIndicator.startAnimating()
             PhotoImages.sharedInstance.getImage(photo, complete: {[weak self] (image, imageFileName) -> () in
@@ -34,5 +39,12 @@ class PhotoViewController: UIViewController {
         } else {
             imageView.image = noPhotoImage
         }
+    }
+
+  }
+
+extension PhotoViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
     }
 }
